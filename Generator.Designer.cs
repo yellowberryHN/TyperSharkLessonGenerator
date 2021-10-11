365a6f6bc2ead2fa6510b3cd337162a94c0d5050
Zsolt Zitting <yellowberry@cocaine.ninja>
2021-10-11 09:40:25 -0600

    Initial code, sucks. I made this while sleep deprived.

---
+++ Generator.Designer.cs
@@ -0,0 +1,340 @@
+﻿
+namespace TyperSharkLessonGenerator
+{
+    partial class Generator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.lessonSelect = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.minAccuracyChk = new System.Windows.Forms.CheckBox();
+            this.minAccuracyBox = new System.Windows.Forms.NumericUpDown();
+            this.minWpmChk = new System.Windows.Forms.CheckBox();
+            this.minWpmBox = new System.Windows.Forms.NumericUpDown();
+            this.maxErrorsChk = new System.Windows.Forms.CheckBox();
+            this.maxErrorsBox = new System.Windows.Forms.NumericUpDown();
+            this.lessonTimeChk = new System.Windows.Forms.CheckBox();
+            this.lessonTimeBox = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.descInput = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minAccuracyBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minWpmBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxErrorsBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lessonTimeBox)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(6, 22);
+            this.textInput.Multiline = true;
+            this.textInput.Name = "textInput";
+            this.textInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textInput.Size = new System.Drawing.Size(388, 389);
+            this.textInput.TabIndex = 0;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(100, 22);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(88, 23);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Enabled = false;
+            this.loadBtn.Location = new System.Drawing.Point(6, 22);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(88, 23);
+            this.loadBtn.TabIndex = 2;
+            this.loadBtn.Text = "Load";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            // 
+            // lessonSelect
+            // 
+            this.lessonSelect.FormattingEnabled = true;
+            this.lessonSelect.Items.AddRange(new object[] {
+            "Typing Test",
+            "Lesson 1",
+            "Lesson 2",
+            "Lesson 3",
+            "Lesson 4",
+            "Lesson 5",
+            "Lesson 6",
+            "Lesson 7",
+            "Lesson 8",
+            "Lesson 9",
+            "Lesson 10",
+            "Lesson 11",
+            "Lesson 12",
+            "Lesson 13",
+            "Lesson 14",
+            "Lesson 15",
+            "Lesson 16",
+            "Lesson 17",
+            "Lesson 18"});
+            this.lessonSelect.Location = new System.Drawing.Point(6, 82);
+            this.lessonSelect.Name = "lessonSelect";
+            this.lessonSelect.Size = new System.Drawing.Size(182, 23);
+            this.lessonSelect.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textInput);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 417);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Raw Text";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.minAccuracyChk);
+            this.groupBox2.Controls.Add(this.minAccuracyBox);
+            this.groupBox2.Controls.Add(this.minWpmChk);
+            this.groupBox2.Controls.Add(this.minWpmBox);
+            this.groupBox2.Controls.Add(this.maxErrorsChk);
+            this.groupBox2.Controls.Add(this.maxErrorsBox);
+            this.groupBox2.Controls.Add(this.lessonTimeChk);
+            this.groupBox2.Controls.Add(this.lessonTimeBox);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.loadBtn);
+            this.groupBox2.Controls.Add(this.saveBtn);
+            this.groupBox2.Controls.Add(this.lessonSelect);
+            this.groupBox2.Location = new System.Drawing.Point(418, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(194, 217);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Options";
+            // 
+            // minAccuracyChk
+            // 
+            this.minAccuracyChk.AutoSize = true;
+            this.minAccuracyChk.Location = new System.Drawing.Point(6, 187);
+            this.minAccuracyChk.Name = "minAccuracyChk";
+            this.minAccuracyChk.Size = new System.Drawing.Size(120, 19);
+            this.minAccuracyChk.TabIndex = 12;
+            this.minAccuracyChk.Text = "Min Accuracy (%)";
+            this.minAccuracyChk.UseVisualStyleBackColor = true;
+            this.minAccuracyChk.CheckedChanged += new System.EventHandler(this.minAccuracyChk_CheckedChanged);
+            // 
+            // minAccuracyBox
+            // 
+            this.minAccuracyBox.Enabled = false;
+            this.minAccuracyBox.Location = new System.Drawing.Point(143, 186);
+            this.minAccuracyBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.minAccuracyBox.Name = "minAccuracyBox";
+            this.minAccuracyBox.Size = new System.Drawing.Size(45, 23);
+            this.minAccuracyBox.TabIndex = 11;
+            this.minAccuracyBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // minWpmChk
+            // 
+            this.minWpmChk.AutoSize = true;
+            this.minWpmChk.Location = new System.Drawing.Point(6, 162);
+            this.minWpmChk.Name = "minWpmChk";
+            this.minWpmChk.Size = new System.Drawing.Size(79, 19);
+            this.minWpmChk.TabIndex = 10;
+            this.minWpmChk.Text = "Min WPM";
+            this.minWpmChk.UseVisualStyleBackColor = true;
+            this.minWpmChk.CheckedChanged += new System.EventHandler(this.minWpmChk_CheckedChanged);
+            // 
+            // minWpmBox
+            // 
+            this.minWpmBox.Enabled = false;
+            this.minWpmBox.Location = new System.Drawing.Point(143, 161);
+            this.minWpmBox.Maximum = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.minWpmBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.minWpmBox.Name = "minWpmBox";
+            this.minWpmBox.Size = new System.Drawing.Size(45, 23);
+            this.minWpmBox.TabIndex = 9;
+            this.minWpmBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // maxErrorsChk
+            // 
+            this.maxErrorsChk.AutoSize = true;
+            this.maxErrorsChk.Location = new System.Drawing.Point(6, 137);
+            this.maxErrorsChk.Name = "maxErrorsChk";
+            this.maxErrorsChk.Size = new System.Drawing.Size(82, 19);
+            this.maxErrorsChk.TabIndex = 8;
+            this.maxErrorsChk.Text = "Max Errors";
+            this.maxErrorsChk.UseVisualStyleBackColor = true;
+            this.maxErrorsChk.CheckedChanged += new System.EventHandler(this.maxErrorsChk_CheckedChanged);
+            // 
+            // maxErrorsBox
+            // 
+            this.maxErrorsBox.Enabled = false;
+            this.maxErrorsBox.Location = new System.Drawing.Point(143, 136);
+            this.maxErrorsBox.Name = "maxErrorsBox";
+            this.maxErrorsBox.Size = new System.Drawing.Size(45, 23);
+            this.maxErrorsBox.TabIndex = 7;
+            this.maxErrorsBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lessonTimeChk
+            // 
+            this.lessonTimeChk.AutoSize = true;
+            this.lessonTimeChk.Location = new System.Drawing.Point(6, 112);
+            this.lessonTimeChk.Name = "lessonTimeChk";
+            this.lessonTimeChk.Size = new System.Drawing.Size(91, 19);
+            this.lessonTimeChk.TabIndex = 6;
+            this.lessonTimeChk.Text = "Lesson Time";
+            this.lessonTimeChk.UseVisualStyleBackColor = true;
+            this.lessonTimeChk.CheckedChanged += new System.EventHandler(this.lessonTimeChk_CheckedChanged);
+            // 
+            // lessonTimeBox
+            // 
+            this.lessonTimeBox.Enabled = false;
+            this.lessonTimeBox.Location = new System.Drawing.Point(143, 111);
+            this.lessonTimeBox.Maximum = new decimal(new int[] {
+            6000,
+            0,
+            0,
+            0});
+            this.lessonTimeBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.lessonTimeBox.Name = "lessonTimeBox";
+            this.lessonTimeBox.Size = new System.Drawing.Size(45, 23);
+            this.lessonTimeBox.TabIndex = 5;
+            this.lessonTimeBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Lesson:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.descInput);
+            this.groupBox3.Location = new System.Drawing.Point(418, 235);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(194, 194);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Description";
+            // 
+            // descInput
+            // 
+            this.descInput.Location = new System.Drawing.Point(6, 22);
+            this.descInput.Multiline = true;
+            this.descInput.Name = "descInput";
+            this.descInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.descInput.Size = new System.Drawing.Size(182, 166);
+            this.descInput.TabIndex = 0;
+            // 
+            // Generator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Generator";
+            this.Text = "Typer Shark Lesson Generator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minAccuracyBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minWpmBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxErrorsBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lessonTimeBox)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.ComboBox lessonSelect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox lessonTimeChk;
+        private System.Windows.Forms.NumericUpDown lessonTimeBox;
+        private System.Windows.Forms.CheckBox maxErrorsChk;
+        private System.Windows.Forms.NumericUpDown maxErrorsBox;
+        private System.Windows.Forms.CheckBox minWpmChk;
+        private System.Windows.Forms.NumericUpDown minWpmBox;
+        private System.Windows.Forms.CheckBox minAccuracyChk;
+        private System.Windows.Forms.NumericUpDown minAccuracyBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox descInput;
+    }
+}
+
---
+++ Generator.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TyperSharkLessonGenerator
+{
+    public partial class Generator : Form
+    {
+        public Generator()
+        {
+            InitializeComponent();
+            lessonSelect.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Word wraps the given text to fit within the specified width.
+        /// </summary>
+        /// <param name="text">Text to be word wrapped</param>
+        /// <param name="width">Width, in characters, to which the text
+        /// should be word wrapped</param>
+        /// <returns>The modified text</returns>
+        public static string WordWrap(string text, int width)
+        {
+            int pos, next;
+            StringBuilder sb = new StringBuilder();
+
+            // Lucidity check
+            if (width < 1)
+                return text;
+
+            // Parse each line of text
+            for (pos = 0; pos < text.Length; pos = next)
+            {
+                // Find end of line
+                int eol = text.IndexOf(Environment.NewLine, pos);
+                if (eol == -1)
+                    next = eol = text.Length;
+                else
+                    next = eol + Environment.NewLine.Length;
+
+                // Copy this line of text, breaking into smaller lines as needed
+                if (eol > pos)
+                {
+                    do
+                    {
+                        int len = eol - pos;
+                        if (len > width)
+                            len = BreakLine(text, pos, width);
+                        sb.Append(text, pos, len);
+                        sb.Append(Environment.NewLine);
+
+                        // Trim whitespace following break
+                        pos += len;
+                        while (pos < eol && Char.IsWhiteSpace(text[pos]))
+                            pos++;
+                    } while (eol > pos);
+                }
+                else sb.Append(Environment.NewLine); // Empty line
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Locates position to break the given line so as to avoid
+        /// breaking words.
+        /// </summary>
+        /// <param name="text">String that contains line of text</param>
+        /// <param name="pos">Index where line of text starts</param>
+        /// <param name="max">Maximum line length</param>
+        /// <returns>The modified line length</returns>
+        private static int BreakLine(string text, int pos, int max)
+        {
+            // Find last whitespace in line
+            int i = max;
+            while (i >= 0 && !Char.IsWhiteSpace(text[pos + i]))
+                i--;
+
+            // If no whitespace found, break at maximum length
+            if (i < 0)
+                return max;
+
+            // Find start of whitespace
+            while (i >= 0 && Char.IsWhiteSpace(text[pos + i]))
+                i--;
+
+            // Return length of text before whitespace
+            return i + 1;
+        }
+
+        /*
+            <LessonTime>-1</LessonTime>
+            <MaxErrors>-1</MaxErrors>
+            <MinWPM>-1</MinWPM>
+            <MinAccuracy>-1</MinAccuracy>
+        */
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            // TODO: figure out the exact width of each character and compensate for that here
+            // instead of doing blind word wrap
+            string wrapped = WordWrap(textInput.Text, 50);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(string.Concat("<LESSON>", Environment.NewLine, Environment.NewLine, "<DESCRIPTION>", descInput.Text, "</DESCRIPTION>",Environment.NewLine, Environment.NewLine));
+
+            sb.Append(string.Concat("<LessonTime>", lessonTimeChk.Checked ? lessonTimeBox.Value : -1 , "</LessonTime>", Environment.NewLine));
+            sb.Append(string.Concat("<MaxErrors>", maxErrorsChk.Checked ? maxErrorsBox.Value : -1, "</MaxErrors>", Environment.NewLine));
+            sb.Append(string.Concat("<MinWPM>", minWpmChk.Checked ? minWpmBox.Value : -1, "</MinWPM>", Environment.NewLine));
+            sb.Append(string.Concat("<MinAccuracy>", minAccuracyChk.Checked ? minAccuracyBox.Value : -1, "</MinAccuracy>", Environment.NewLine, Environment.NewLine));
+
+            using (StringReader reader = new StringReader(wrapped))
+            {
+                string line;
+                int count = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if(count % 4 == 0) sb.Append(string.Concat("<TASK>",Environment.NewLine));
+                    sb.Append(string.Concat("<TEXT>", line, "</TEXT>", Environment.NewLine));
+                    if(count % 4 == 3) sb.Append(string.Concat("</TASK>",Environment.NewLine, Environment.NewLine));
+                    count++;
+                }
+                if (count % 4 > 0) sb.Append(string.Concat("</TASK>", Environment.NewLine, Environment.NewLine));
+            }
+
+            sb.Append(string.Concat("</LESSON>",Environment.NewLine));
+
+            File.WriteAllText(@$"Lesson{lessonSelect.SelectedIndex}.xml", sb.ToString());
+
+            MessageBox.Show($"Lesson{lessonSelect.SelectedIndex}.xml generated!","Typer Shark Lesson Generator");
+        }
+
+        private void lessonTimeChk_CheckedChanged(object sender, EventArgs e)
+        {
+            lessonTimeBox.Enabled = lessonTimeChk.Checked;
+        }
+
+        private void maxErrorsChk_CheckedChanged(object sender, EventArgs e)
+        {
+            maxErrorsBox.Enabled = maxErrorsChk.Checked;
+        }
+
+        private void minWpmChk_CheckedChanged(object sender, EventArgs e)
+        {
+            minWpmBox.Enabled = minWpmChk.Checked;
+        }
+
+        private void minAccuracyChk_CheckedChanged(object sender, EventArgs e)
+        {
+            minAccuracyBox.Enabled = minAccuracyChk.Checked;
+        }
+    }
+}
